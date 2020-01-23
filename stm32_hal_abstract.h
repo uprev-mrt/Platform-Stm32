186df6b1c32a2c2e8c9ad5371c99ab826d16b945
Jason Berger <berge472@gmail.com>
2020-01-23 12:53:28 -0500

    improved handling of unused buses

--- stm32_hal_abstract.h
+++ stm32_hal_abstract.h
@@ -26,11 +26,13 @@
 #define MRT_DELAY_MS(ms) HAL_Delay(ms)
 
 //Uart Abstraction
+typedef HAL_StatusTypeDef mrt_uart_status_t;
 #ifdef HAL_UART_MODULE_ENABLED
   typedef UART_HandleTypeDef* mrt_uart_handle_t;
-  typedef HAL_StatusTypeDef mrt_uart_status_t;
   #define MRT_UART_TX(handle, data, len, timeout) HAL_UART_Transmit(handle, data, len, timeout)
   #define MRT_UART_RX(handle, data, len, timeout) HAL_UART_Retrieve(handle, data, len, timeout)
+#else
+  typedef uint32_t* mrt_uart_handle_t;
 #endif
 
 
@@ -44,22 +46,26 @@ typedef struct{
 #define MRT_GPIO_READ( gpio ) HAL_GPIO_ReadPin(gpio.port, gpio.pin)
 
 //I2C Abstraction
+typedef HAL_StatusTypeDef mrt_i2c_status_t;
 #ifdef HAL_I2C_MODULE_ENABLED
   typedef I2C_HandleTypeDef* mrt_i2c_handle_t;
-  typedef HAL_StatusTypeDef mrt_i2c_status_t;
   #define MRT_I2C_MASTER_TRANSMIT(handle ,addr,data,len, stop, timeout) HAL_I2C_Master_Transmit(handle , addr, data, len, timeout)
   #define MRT_I2C_MASTER_RECEIVE(handle ,addr, data, len, stop, timeout) HAL_I2C_Master_Receive(handle , addr, data, len, timeout)
   #define MRT_I2C_MEM_WRITE(handle, addr, mem_addr, mem_size, data, len, timeout ) HAL_I2C_Mem_Write(handle ,addr, mem_addr, mem_size, data, len, timeout)
   #define MRT_I2C_MEM_READ(handle, addr, mem_addr, mem_size, data, len, timeout ) HAL_I2C_Mem_Read(handle ,addr, mem_addr, mem_size, data, len, timeout)
+#else
+  typedef uint32_t* mrt_i2c_handle_t;
 #endif
 
 //SPI Abstraction
+typedef HAL_StatusTypeDef mrt_spi_status_t;
 #ifdef HAL_SPI_MODULE_ENABLED
   typedef SPI_HandleTypeDef* mrt_spi_handle_t;
-  typedef HAL_StatusTypeDef mrt_spi_status_t;
   #define MRT_SPI_TRANSFER(handle ,tx, rx ,len, timeout) HAL_SPI_TransmitReceive(handle, tx, rx, len, timeout)
   #define MRT_SPI_TRANSMIT(handle, tx, len, timeout) HAL_SPI_Transmit(handle, tx, len, timeout)
   #define MRT_SPI_RECIEVE(handle, tx, len, timeout) HAL_SPI_Transmit(handle, tx, len, timeout)
+#else
+  typedef uint32_t* mrt_spi_handle_t;
 #endif
 
 //printf
