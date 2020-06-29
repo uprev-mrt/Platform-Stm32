4314234d3a0febdacba70166e2d856c11b659603
Jason Berger <berge472@gmail.com>
2020-06-29 10:43:29 -0400

    adding flash memory control

---
+++ flash/flash_mem.c
@@ -0,0 +1,51 @@
+/**
+ * @file flash_memory.c
+ * @author Jason Berger
+ * @brief Library for accessing flash data
+ * @date 2020-06-23
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#pragma once 
+
+/* Includes ------------------------------------------------------------------*/
+#include "flash_mem.h"
+
+/* Private Variables ---------------------------------------------------------*/
+/* Functions -----------------------------------------------------------------*/
+
+mrt_status_t flash_init(flash_chunk_t* fc, uint32_t baseAddr, uint32_t chunkSize, uint32_t pageSize )
+{
+    fc->mBaseAddr = baseAddr;
+    fc->mChunkSize = chunkSize;
+    fc->mPageSize = pageSize;
+    fc->mCursor = 0;
+    return MRT_STATUS_OK;
+}
+
+mrt_status_t flash_write(flash_chunk_t* fc, uint32_t addr, const uint8_t* data, uint32_t len)
+{
+    uint32_t* cast = (uint16_t*)data;
+
+    HAL_FLASH_Unlock();
+
+    for(int i=0; i < len; i+=4)
+    {
+        HAL_FLASH_Program(FLASH_PROC_PROGRAMWORD, fc->mBaseAddr + addr + i, cast[i]);
+        fc->mCursor = fc->mBaseAddr + addr + i +1;
+    }
+
+    HAL_FLASH_Lock();
+
+    return MRT_STATUS_OK;
+}
+
+mrt_status_t flash_read(flash_chunk_t* fc, uint32_t addr, uint8_t* data, uint32_t len)
+{
+    uint8_t* flashData = (*(__IO uint8_t*)(fc->mBaseAddr + addr));
+    memcpy(data, flashData, len);
+    
+    return MRT_STATUS_OK;
+}
---
+++ flash/flash_mem.h
@@ -0,0 +1,56 @@
+/**
+ * @file flash_memory.h
+ * @author Jason Berger
+ * @brief Library for accessing flash data
+ * @date 2020-06-23
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#pragma once 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "Platforms/Common/mrt_platform.h"
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+typedef struct{
+    uint32_t mBaseAddr;  //Start address for this chunk  
+    uint32_t mChunkSize; //Size of this chunk in bytes
+    uint32_t mPageSize;  //Page size of memory
+    uint32_t mCursor;    //cursor for next write/read
+}flash_chunk_t;
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+/**
+ * @brief initialize flash memory chunk
+ * @param fc ptr to flash chunk struct
+ * @param baseAddr base address of chunk
+ * @param chunkSize size of chunk
+ * @param pageSize page size for physical memory
+ * @return mrt_status_t 
+ */
+mrt_status_t flash_init(flash_chunk_t* fc, uint32_t baseAddr, uint32_t chunkSize, uint32_t pageSize );
+
+/**
+ * @brief Writes data to flash memory chunk
+ * @param fc ptr to flash chunk struct
+ * @param addr address offset from chunks base Address to write to (in bytes ) 
+ * @param len number of bytes to write
+ * @param data ptr to data to write
+ * @return mrt_status_t 
+ */
+mrt_status_t flash_write(flash_chunk_t* fc, uint32_t addr, const uint8_t* data, uint32_t len);
+
+/**
+ * @brief Reads data from flash memory chunk
+ * @param fc ptr to flash chunk struct
+ * @param addr address offset from chunks base Address to read from (in bytes ) 
+ * @param len number of bytes to read
+ * @param data ptr to store data
+ * @return mrt_status_t 
+ */
+mrt_status_t flash_read(flash_chunk_t* fc, uint32_t addr, uint8_t* data, uint32_t len);
